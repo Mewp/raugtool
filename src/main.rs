5a68a8f1fa41f0ab60a31a7ab81a71461b92def3
Błażej Święcicki <blazej.swiecicki@pixers.pl>
2017-01-23 17:49:29 +0100

    Fix reorder not to use stale matches.

--- src/main.rs
+++ src/main.rs
@@ -21,18 +21,36 @@ mod augcmd {
                 if matches.len() == 0 {
                     return Ok("".into())
                 }
-                let dest = if before {
-                    matches[0].clone()
-                } else {
-                    matches.last().unwrap().clone()
+                let dest = match before {
+                    true => matches[0].clone(),
+                    false => matches.last().unwrap().clone()
                 };
-                for src in aug.matches(&from).unwrap() {
+                // First, store all the values
+                let mut values: Vec<_> = aug.matches(&from)?.iter().map(|m| aug.get(m)).collect::<Result<_,_>>()?;
+                // Next, replace all of them with a marker value,
+                // because setm is the only thing we can do on multiple
+                // nodes at once
+                aug.setm(&from, None, "raugtool-marker-value")?;
+                // Now for each marked node...
+                for val in values {
+                    // ...we mark it with a different value,
+                    let marker_path = match before {
+                        true => "//*[. = \"raugtool-marker-value\"][1]",
+                        false => "//*[. = \"raugtool-marker-value\"][last()]"
+                    };
+                    aug.set(marker_path, "raugtool-marker-value-current")?;
+                    // move it to the right place,
+                    let src = "//*[. = \"raugtool-marker-value-current\"][1]";
                     let mut marker = dest.clone();
                     marker.push_str("/../augtool-marker-node");
-                    let label = aug.label(&src)?.unwrap();
+                    let label = aug.label(src)?.unwrap();
                     aug.insert(&dest, "augtool-marker-node", before)?;
-                    aug.mv(&src, &marker)?;
+                    aug.mv(src, &marker)?;
+                    // and finally, restore the old value (and name).
+                    aug.set(&marker, val.as_ref().map(|v| v.as_str()))?;
                     aug.rename(&marker, &label)?;
+                    // This way, the first node with the marker value is again the first one to
+                    // move, and so we can move around things without worrying about stale matches.
                 }
                 Ok("".into())
             }
